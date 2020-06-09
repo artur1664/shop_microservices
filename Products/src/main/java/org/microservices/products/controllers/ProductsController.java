9e4054247f3fafac8f782966570ad31ba2486a83
artur <arturpietrucha90@gmail.com>
2020-06-09 17:29:41 +0200

    develop crud operations in reactive controller (how to handle not found etc.?

--- Products/src/main/java/org/microservices/products/controllers/ProductsController.java
+++ Products/src/main/java/org/microservices/products/controllers/ProductsController.java
@@ -1,15 +1,12 @@
 package org.microservices.products.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.microservices.products.model.Product;
 import org.microservices.products.model.dto.ProductDto;
 import org.microservices.products.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.math.BigDecimal;
 
 /**
  * Products reactive controller
@@ -26,14 +23,33 @@ public class ProductsController {
         this.productService = productService;
     }
 
-
     @PostMapping
-    public Mono<ProductDto> addNewProduct(@RequestBody ProductDto productDto){
+    public Mono<Void> addNewProduct(@RequestBody ProductDto productDto){
+        log.info("add new product controller");
         return productService.addNewProduct(productDto);//todo response ?
     }
 
     @GetMapping
     public Flux<ProductDto> getAll(){
+        log.info("get all products controller");
         return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ProductDto> getById(@PathVariable String id){
+        log.info("get product by id controller");
+        return productService.getById(id);
+    }
+
+    @PutMapping
+    public Mono<Void> updateProduct(@RequestBody ProductDto productDto){
+        log.info("update product id controller");
+        return productService.updateProduct(productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteById(@PathVariable String id){
+        log.info("delete product by id controller");
+        return productService.deleteProduct(id);
     }
 }
--- Products/src/main/java/org/microservices/products/services/classes/ProductServiceImpl.java
+++ Products/src/main/java/org/microservices/products/services/classes/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package org.microservices.products.services.classes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.microservices.products.model.Product;
 import org.microservices.products.model.dto.ProductDto;
 import org.microservices.products.model.mappers.ProductMapper;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
 
@@ -23,14 +25,35 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Mono<ProductDto> addNewProduct(ProductDto productDto) {
-        Mono<Product> result = productRepository.save(mapper.dtoToProduct(productDto));
-        return result.map(mapper::productToDto);
+    public Mono<Void> addNewProduct(ProductDto productDto) {
+        log.info("create new product");
+        return productRepository.save(mapper.dtoToProduct(productDto)).then();
     }
 
     @Override
     public Flux<ProductDto> getAll(){
+        log.info("create get all products");
         Flux<Product> result = productRepository.findAll();
         return result.map(mapper::productToDto);
+    }
+
+    @Override
+    public Mono<ProductDto> getById(String id) {
+        log.info("get product by id");
+        return productRepository.findById(id).map(mapper::productToDto);
+    }
+
+    @Override
+    public Mono<Void> deleteProduct(String id){
+        log.info("delete product");
+        return productRepository.deleteById(id);
+    }
+
+    @Override
+    public Mono<Void> updateProduct(ProductDto productDto){
+        Product product = mapper.dtoToProduct(productDto);
+
+        return productRepository.save(product).then();
+
     }
 }
