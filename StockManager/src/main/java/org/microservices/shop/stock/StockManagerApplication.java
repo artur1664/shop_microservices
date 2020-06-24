a07be0b4371078d66bca6d94eb174a954fced6ed
artur <arturpietrucha90@gmail.com>
2020-06-24 17:57:34 +0200

    develop added eureka server and config to clients

--- StockManager/src/main/java/org/microservices/shop/stock/StockManagerApplication.java
+++ StockManager/src/main/java/org/microservices/shop/stock/StockManagerApplication.java
@@ -2,10 +2,12 @@ package org.microservices.shop.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.schema.client.EnableSchemaRegistryClient;
 
 @SpringBootApplication
 @EnableSchemaRegistryClient
+@EnableEurekaClient
 public class StockManagerApplication {
 
     public static void main(String[] args) {
