bf8d6df86f6e2d0c6fdeb31a5eaffa3a0d2d95ee
artur <arturpietrucha90@gmail.com>
2020-06-26 21:02:13 +0200

    develop added shoping service

---
+++ ShopingService/src/main/java/org/microservices/shop/shoping/ShopingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.microservices.shop.shoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopingServiceApplication.class, args);
+    }
+
+}
---
+++ ShopingService/src/test/java/org/microservices/shop/shoping/ShopingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microservices.shop.shoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
