bf62396c55c29538c796d9c07355440afa9c41a1
artur <artur.pietrucha02@gmail.com>
2020-07-07 20:34:28 +0200

    develop even more fixes todo refactor tests to ignore config server

--- EurekaRegistry/src/test/java/org/microservices/shop/registry/EurekaRegistryApplicationTests.java
+++ EurekaRegistry/src/test/java/org/microservices/shop/registry/EurekaRegistryApplicationTests.java
@@ -3,7 +3,6 @@ package org.microservices.shop.registry;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EurekaRegistryApplicationTests {
 
     @Test
