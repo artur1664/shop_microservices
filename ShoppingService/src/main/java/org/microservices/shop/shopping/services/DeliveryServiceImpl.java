a07a8717596e62b58eae3be7dfa11fa471f8480a
artur <artur.pietrucha02@gmail.com>
2020-07-08 17:48:52 +0200

    develop implemented simple example for circuit breaker with resilient4j

--- ShoppingService/src/main/java/org/microservices/shop/shopping/services/DeliveryServiceImpl.java
+++ ShoppingService/src/main/java/org/microservices/shop/shopping/services/DeliveryServiceImpl.java
@@ -1,5 +1,8 @@
 package org.microservices.shop.shopping.services;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
 import lombok.extern.slf4j.Slf4j;
 import org.microservices.shop.shopping.mappers.OrderMapper;
 import org.microservices.shop.shopping.model.Order;
@@ -18,15 +21,15 @@ import java.util.UUID;
 public class DeliveryServiceImpl implements DeliveryService {
 
     private final WebClient client;
-
     private final OrderRepository orderRepository;
-
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
     private final OrderMapper mapper = OrderMapper.INSTANCE;
 
     @Autowired
-    public DeliveryServiceImpl(WebClient client, OrderRepository orderRepository) {
+    public DeliveryServiceImpl(WebClient client, OrderRepository orderRepository, CircuitBreakerRegistry circuitBreakerRegistry) {
         this.client = client;
         this.orderRepository = orderRepository;
+        this.circuitBreakerRegistry = circuitBreakerRegistry;
     }
 
     /**
@@ -35,14 +38,25 @@ public class DeliveryServiceImpl implements DeliveryService {
      */
     @Override
     public Mono<String> sendDeliveryRequest(UUID orderUuid) {
+        CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker("DELIVERY_SERVICE_BREAKER");
+
         Mono<Order> order = orderRepository.findByOrderUuid(orderUuid);
         log.info("***************************** just before request");
         Mono<String> result = client.post()
                 .uri("/api/v1/delivery")
                 .body(Mono.just(order.map(mapper::orderToDto)), DeliveryDto.class)
                 .retrieve()// other option is exchange
-                .bodyToMono(String.class);
+                .bodyToMono(String.class)
+                .transform(CircuitBreakerOperator.of(circuitBreaker))
+                .onErrorResume(error -> fallbackMethod(error, "do something!"));
         log.info("***************************** just after request");
         return result;
+    }
+
+    private Mono<String> fallbackMethod(Throwable error, String msg) {
+        log.error("something went wrong!", error);
+        log.debug("message for developer {}", msg);
+        log.debug("do some actions from fallback method!");
+        return Mono.just("failed!");
     }
 }
